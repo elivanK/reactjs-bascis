0acb98f9a4441e7b847c93630170eede2cad2a49
Elivan Kook <elivanmetal@gmail.com>
2018-01-26 14:54:50 +0100

    initial commit, setup finished

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [{
+            test: /\.js?/,
+            include: SRC_DIR,
+            loader: "babel-loader",
+            query: {
+                presets: ["react", "es2015", "stage-2"]
+            }
+        }]
+    }
+};
+
+module.exports = config;
